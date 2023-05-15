11c190ad2a373a4ebffa7a60b3e47b6f61c066e0
ChesssEnjoyer <kacperstawski83@gmail.com>
2023-05-15 20:01:21 +0200

    Add disk size file

---
+++ disk_size.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python3
+
+import shutil
+
+total, used, free = shutil.disk_usage("/")
+
+print("Total: %d GiB" % (total // (2**30)))
+print("Used: %d GiB" % (used // (2**30)))
+print("Free: %d GiB" % (free // (2**30)))
