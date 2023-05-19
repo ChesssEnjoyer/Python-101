e1fd2d0e5b482285a6ea2d6338c2ca0bb951b8c4
ChesssEnjoyer <kacperstawski83@gmail.com>
2023-05-19 10:30:01 +0200

    Udpate crud.py

--- projects/crud/crud.py
+++ projects/crud/crud.py
@@ -10,8 +10,6 @@ user_name = 'root'
 user_password = 'Ubuntu12'
 db_name = 'local_base'
 
-
-
 def create_server_connection(host_name, user_name, user_password, db_name):
     connection = None
     try: 
@@ -30,14 +28,48 @@ def operation(connection, query):
     except Error as err:
         print(f"Error: '{err}'")
 
+def read(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for result in results:
+            print(result)
+    except Error as err:
+        print(f"Error: '{err}'")
+
 def main():
-    connect =  create_server_connection(host_name, user_name, user_password, db_name)
+    connect = create_server_connection(host_name, user_name, user_password, db_name)
     while True:
-        answer = input('Do you want to do something with database?(y/n) ')
+        answer = input('Do you want to do something with database?(y/n) ').lower()
         if answer == 'n':
             break
-        query = input('Type in query for mysql to execute ')
-        operation(connect, query)
-
+        option = input('What do you wnat do to(c/r/u/d)? ').lower()
+        if option == 'r':
+            first = input('What do you want to select? ')
+            second = input('From what table? ')
+            query = (f'SELECT {first} FROM {second} ;')
+            read(connect, query)
+        elif option == 'c':
+            first = input('What table do you want to create? ')
+            second = input('What are columns (column_name TYPE)? ')
+            query = (f'CREATE TABLE {first} ({second});')
+            operation(connect, query)
+        elif option == 'u':
+            first = input('What table do you want to update? ')
+            second = input('What do you want to update (column_name = "value")? ')
+            third = input('For which record (WHERE clausule)? ')
+            query = (f'UPDATE {first} SET {second} WHERE {third};')
+            operation(connect, query)
+            connect.commit()
+        elif option == 'd':
+            first = input('In what table do you want to delete smth? ')
+            second = input('For which record (WHERE clausule)? ')        
+            query = (f'DELETE FROM {first} WHERE {second}')
+            operation(connect, query)
+            connect.commit()
+        else:
+            print('Wybierz poprawna opcje!!!')
 main()
 
